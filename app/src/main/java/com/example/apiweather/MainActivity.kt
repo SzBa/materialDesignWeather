3462c66cea5ce0c84c56810ec073de6e54699363
SzBa <48062979+SzBa@users.noreply.github.com>
2020-03-21 12:48:47 +0100

    UI is done

--- app/src/main/java/com/example/apiweather/MainActivity.kt
+++ app/src/main/java/com/example/apiweather/MainActivity.kt
@@ -2,41 +2,37 @@ package com.example.apiweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.google.gson.Gson
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import okhttp3.*
-import java.io.IOException
-import java.lang.Exception
-import java.net.CacheResponse
-import java.net.URL
-import java.nio.charset.Charset
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        JsonFun()
+        imageView.setImageResource(R.drawable.ic_wi_day_cloudy)
+        imageViewThermometer.setImageResource(R.drawable.ic_wi_thermometer)
+        imageViewDegrees.setImageResource(R.drawable.ic_wi_degrees)
+        imageViewBarometer.setImageResource(R.drawable.ic_wi_barometer)
+
+        //fetchJson()
 
     }
+}
 
+/*
+    fun fetchJson() {
 
-    fun JsonFun() {
+        val view:TextView = findViewById(R.id.textDateView)
 
-        val city = "Gliwice,pl"
+        var city:String = "Gliwice"
 
         val API_KEY = "4d9c1e57310a8c80e27c9d8b1e80c77b"
 
         //https://api.openweathermap.org/data/2.5/weather?q=Gliwice,pl&APPID=4d9c1e57310a8c80e27c9d8b1e80c77b
         val url =
             "https://api.openweathermap.org/data/2.5/weather?q=$city&APPID=$API_KEY"
-
-
 
         val request = Request.Builder().url(url).build()
 
@@ -49,9 +45,61 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call, response: Response) {
                 val body = response?.body?.string()
                 println(body)
-            }
 
+                val gson = GsonBuilder().create()
+
+                val homeWeather = gson.fromJson(body, WeatherDataClass::class.java)
+                println(homeWeather)
+
+                val homeWeather2 = gson.fromJson(body, Main::class.java)
+                println(homeWeather2)
+
+                val homeWeather3 = gson.fromJson(body, Sys::class.java)
+                println(homeWeather3)
+
+                checkButton.setOnClickListener {
+                    textTemperatureView.text = homeWeather.temp.toString()
+                    textPressureView.text = homeWeather2.pressure.toString()
+                    textSunriseView.text = homeWeather.sunrise.toString()
+                    textSunsetView.text = homeWeather.sunset.toString()
+                    textTimeView.text = homeWeather.timezone.toString()
+                    textDescriptionView.text = homeWeather.description
+                }
+            }
         })
     }
-
 }
+
+
+// data -> nie ma
+
+data class WeatherDataClass(
+    val name: String, //nazwa miejscowosci, mozliwosc wpisania w pole
+    val weather: List<Weather>, //potrzebne -> description, ikona
+    val timezone: Int,
+    val pressure: Int, //cisnienie
+    val temp: Double, // temperatura
+    val sunrise: Int, //wschod
+    val sunset: Int, //zachod
+    val description: String
+)
+
+data class Main(
+    val pressure: Int, //cisnienie
+    val temp: Double // temperatura
+)
+
+data class Sys(
+    val sunrise: Int, //wschod
+    val sunset: Int //zachod
+)
+
+data class Weather(
+    val description: String,
+    val icon: String,
+    val id: Int,
+    val main: String
+)
+
+
+*/
